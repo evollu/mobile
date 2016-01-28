f6bbed6b0b835e4b68de38b453853823b08b81b7
Libin Lu <llu@evolenthealth.com>
2016-01-28 15:49:17 -0500

    jasmine create spy obj

--- app/pages/cart/test/cart.spec.ts
+++ app/pages/cart/test/cart.spec.ts
@@ -5,5 +5,7 @@ import {Page, NavController} from 'ionic-framework/ionic';
 describe('CartPage', ()=>{
     it('should', ()=>{
         let cartService = new CartService();
+        let nav = jasmine.createSpyObj('NavController', ['nav']);
+        let cart = new CartPage(cartService, nav);
     })    
 })
